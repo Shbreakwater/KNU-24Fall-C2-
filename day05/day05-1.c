1d576a5d358d4961c7300c9e472794ea79ef4adf
Shbreakwater <kimmq400@gmail.com>
2024-09-26 16:17:23 +0900

    동적 메모리
    +최대,최소,평균 추가

--- day05/day05-1.c
+++ day05/day05-1.c
@@ -1,42 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int n;
-    int id;
-    char name[50];
-    float score, max, min, sum = 0;
+    int N;
+    int* snum;
+    char** name;
+    int* score;
+    int max, min;
+    float sum = 0, avg;
 
+    
     printf("학생 수 입력: ");
-    scanf_s("%d", &n);
+    scanf_s("%d", &N);
 
-    printf("학생 1의 학번 입력: ");
-    scanf_s("%d", &id);
-
-    printf("학생 1의 이름 입력: ");
-    scanf_s("%s", name);
-
-    printf("학생 1의 점수 입력: ");
-    scanf_s("%f", &score);
-    max = min = score;
-    sum = score;
-    for (int i = 2; i <= n; i++) {
-        printf("\n학생 %d의 학번 입력: ", i);
-        scanf_s("%d", &id);
-
-        printf("학생 %d의 이름 입력: ", i);
-        scanf_s("%s", name);
-
-        printf("학생 %d의 점수 입력: ", i);
-        scanf_s("%f", &score);
-
-        if (score > max) max = score;
-        if (score < min) min = score;
-        sum += score;
+    snum = (int*)malloc(N * sizeof(int));
+    score = (int*)malloc(N * sizeof(int));
+    name = (char**)malloc(N * sizeof(char*));
+    for (int i = 0; i < N; i++) {
+        name[i] = (char*)malloc(100 * sizeof(char));
     }
-    float average = sum / n;
-    printf("\n최고 점수: %.2f\n", max);
-    printf("최저 점수: %.2f\n", min);
-    printf("평균 점수: %.2f\n", average);
+
+    for (int i = 0; i < N; i++) {
+        printf("학번: ");
+        scanf_s("%d", &snum[i]);
+
+        printf("이름: ");
+        scanf_s("%s", name[i], 100);
+
+        printf("점수: ");
+        scanf_s("%d", &score[i]);
+
+        
+        sum += score[i];
+
+        
+        if (i == 0) {
+            max = min = score[i];
+        }
+        else {
+            if (score[i] > max) max = score[i];
+            if (score[i] < min) min = score[i];
+        }
+    }
+
+    avg = sum / N;
+
+    printf("\n학생 정보:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d %s %d\n", snum[i], name[i], score[i]);
+    }
+
+    printf("\n최고 점수: %d\n", max);
+    printf("최저 점수: %d\n", min);
+    printf("평균 점수: %.2f\n", avg);
+
+    free(snum);
+    for (int i = 0; i < N; i++) {
+        free(name[i]);
+    }
+    free(name);
+    free(score);
 
     return 0;
 }
