1a23b11cd15692d8cf4c40fd2911580d12bd8397
Shbreakwater <kimmq400@gmail.com>
2024-10-07 16:50:10 +0900

    연결리스트
    중간 노드 삭제하고 출력하는 것 구현

---
+++ Day06/day06-1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* fourth = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* fifth = (struct Node*)malloc(sizeof(struct Node));
+
+    head->data = 1; head->next = second;
+    second->data = 2; second->next = third;
+    third->data = 3; third->next = fourth;
+    fourth->data = 4; fourth->next = fifth;
+    fifth->data = 5; fifth->next = NULL;
+
+    printf("연결 리스트: ");
+    printList(head);
+
+    second->next = fourth;
+    free(third);
+
+    printf("3번째 노드를 삭제한 후 연결 리스트: ");
+    printList(head);
+
+    return 0;
+}
