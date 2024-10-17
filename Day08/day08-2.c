abdf5a24c2297c21482fdcd81b0b7eb77a64f078
Shbreakwater <kimmq400@gmail.com>
2024-10-17 13:55:16 +0900

    포인터 수정
    포인터 수정본

--- Day08/day08-2.c
+++ Day08/day08-2.c
@@ -2,52 +2,59 @@
 #include <stdlib.h>
 #include <time.h>
 
-void initializeAndPrintArray(int (*array)[10]) {
+#define SIZE 10
+
+void fillRandom(void* array) {
     srand(time(0));
+    for (int i = 0; i < SIZE * SIZE; i++) {
+        *((int*)array + i) = rand() % 20 + 1;
+    }
+}
+
+void printArray(void* array) {
     printf("배열 출력:\n");
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            array[i][j] = rand() % 20 + 1;
-            printf("%2d ", array[i][j]);
+    for (int i = 0; i < SIZE * SIZE; i++) {
+        printf("%2d ", *((int*)array + i));
+        if ((i + 1) % SIZE == 0) printf("\n");
+    }
+}
+
+void movePointer(void* array) {
+    int x = 0, y = 0;
+    int value;
+
+    while (1) {
+        int * ptr = (int*)array + x * SIZE + y;
+        value = *ptr;
+        printf("현재 위치: (%d, %d), 배열의 값: %d\n", x, y, value);
+        int newX = x;
+        int newY = y + value;
+        if (newY >= SIZE) {
+            newX += newY / SIZE;
+            newY = newY % SIZE;
         }
-        printf("\n");
+
+        if (newX < SIZE && newY < SIZE) {
+            x = newX;
+            y = newY;
+        }
+        else {
+            break;
+        }
     }
-}
-
-int move(int (*array)[10], int *x, int *y) {
-    int value = *(*(array + *x) + *y);
-    printf("현재 위치: (%d, %d), 배열의 값: %d\n", *x, *y, value);
-
-    int newX = *x;
-    int newY = *y + value;
-
-    if (newY >= 10) {
-        newX += newY / 10;
-        newY = newY % 10;
-    }
-
-    if (newX < 10 && newY < 10) {
-        *x = newX;
-        *y = newY;
-        return 1;
-    }
-    return 0;
-}
-
-void printFinalPosition(int x, int y, int value) {
+    int* finalPtr = (int*)array + x * SIZE + y;
     printf("더 이상 이동할 수 없습니다.\n");
-    printf("종료 위치: (%d, %d), 배열의 값: %d\n", x, y, value);
+    printf("종료 위치: (%d, %d), 배열의 값: %d\n", x, y, *finalPtr);
 }
+
 
 int main() {
-    int array[10][10];
-    int x = 0, y = 0;
+    int array[SIZE][SIZE];
 
-    initializeAndPrintArray(array);
+    fillRandom(array); 
+    printArray(array);
 
-    while (move(array, &x, &y));
-
-    printFinalPosition(x, y, array[x][y]);
+    movePointer(array);
 
     return 0;
 }
